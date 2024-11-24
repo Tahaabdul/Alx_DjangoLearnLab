7b978d1fed01cf8c331b0ffdbe022dc90377e433
Tahaabdul <rhaxaqi@gmail.com>
2024-11-24 11:57:25 +0000

    correction task 1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('librarylist/<int:pk>/',views.LibraryListView.as_view(), name ="librarylist")
+    path('books/', list_books, name='list_books'),
+    path('librarylist/<int:pk>/',LibraryDetailView.as_view(), name ="librarylist")
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,7 +13,7 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', context=context)
     
 
-class LibraryListView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     def get_context_data(self, **kwargs):
